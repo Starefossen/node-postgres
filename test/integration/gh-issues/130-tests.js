8aae0594dad746248069d46c4fe2c45a1c5451dc
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2015-07-26 21:51:19 +0200

    Remove dependency on `psgq` command line utility

--- test/integration/gh-issues/130-tests.js
+++ test/integration/gh-issues/130-tests.js
@@ -1,17 +1,16 @@
 var helper = require(__dirname + '/../test-helper');
-var exec = require('child_process').exec;
 
 helper.pg.defaults.poolIdleTimeout = 1000;
 
 helper.pg.connect(helper.config, function(err,client) {
   client.query("SELECT pg_backend_pid()", function(err, result) {
     var pid = result.rows[0].pg_backend_pid;
-    exec('psql -c "select pg_terminate_backend('+pid+')" template1', assert.calls(function (error, stdout, stderr) {
-        assert.isNull(error);
+    client.query('SELECT pg_terminate_backend('+pid+')', assert.calls(function (err, result) {
+      //assert.isNull(error);
     }));
   });
 });
 
 helper.pg.on('error', function(err, client) {
-  //swallow errors
+  assert.ifError(err);
 });
